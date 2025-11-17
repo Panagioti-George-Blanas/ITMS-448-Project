4d7665313968d3b34ca7ca40afe1b4a85a7ed215
Henry T <htran21@hawk.iit.edu>
2025-11-17 16:38:41 -0600

    main.py edits
    added flask

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,43 +14,6 @@ from clients.omdb_client import OMDBClient
 
 load_dotenv()
 
-<<<<<<< HEAD
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
-=======
-app = Flask(__name__)
-app.secret_key = os.getenv("SESSION_SECRET", "fallback_secret")
-
-spotify = SpotifyClient()
-tmdb = TMDBClient()
-omdb = OMDBClient()
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/login")
-def login():
-    # redirect users to Spotify OAuth
-    return redirect(spotify.get_auth_url())
-
-@app.route("/callback")
-def callback():
-    # Spotify sends back a ?code=
-    code = request.args.get("code")
-    token_info = spotify.get_token(code)
-
-    # store access token in session
-    session["access_token"] = token_info["access_token"]
-    return redirect("/songs")
-
-@app.route("/songs")
-def songs():
-    access_token = session.get("access_token")
-    recent = spotify.get_recent_tracks(access_token)
-    return render_template("results.html", songs=recent)
-
-if __name__ == "__main__":
-    app.run(debug=True)
->>>>>>> fef23299b8eebaf73cc3451b89d416714d0af7db
